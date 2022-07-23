7c0a4da7d49efd5c7b4e2ff3e4f27a75ed8a6dd6
Ishidall <tishido.14670806@gmail.com>
2022-07-23 15:59:11 +0900

    Fix conflict

--- sql/patch.sql
+++ sql/patch.sql
@@ -1,9 +1,3 @@
 USE `isuports`;
 
-<<<<<<< HEAD
-alter table player add index tenant_created_at_idx (tenant_id, created_at DESC);
-
 ALTER TABLE competition ADD player_count INT NOT NULL DEFAULT 0, ADD visitor_count INT NOT NULL DEFAULT 0;
-=======
-
->>>>>>> 0fbcc8dcb0ac5fb29046f6e628749fa5e4a7afc7
