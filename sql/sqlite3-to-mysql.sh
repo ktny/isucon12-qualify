6c8b894913747561817b618e5d7b4e7b36d5e5e3
ktny <katsunori.t10@gmail.com>
2022-07-23 11:22:36 +0900

    fix!

--- sql/sqlite3-to-mysql.sh
+++ sql/sqlite3-to-mysql.sh
@@ -3,8 +3,8 @@ set -x pipefail
 
 for i in {1..234}; do
     echo start $i.db
-    rm -f /tmp/tmp.sql
-    ./sqlite3-to-sql ../tenant_db/$i.db > /tmp/sqlite_$1.sql
+    # rm -f /tmp/tmp.sql
+    ./sqlite3-to-sql ../tenant_db/$i.db > /tmp/tmp.sql
 done
 
-# mysql -uisucon -pisucon isuports < /tmp/tmp.sql
+mysql -uisucon -pisucon isuports < /tmp/tmp.sql
