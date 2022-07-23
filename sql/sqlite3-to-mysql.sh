b0d1cd253b70ded6e31ee16e5766cd584b658f74
ktny <katsunori.t10@gmail.com>
2022-07-23 12:52:01 +0900

    fix

--- sql/sqlite3-to-mysql.sh
+++ sql/sqlite3-to-mysql.sh
@@ -8,9 +8,4 @@ for i in {1..234}; do
     /home/isucon/webapp/sql/sqlite3-to-sql /home/isucon/webapp/tenant_db/$i.db >> /tmp/tmp.sql
 done
 
-# mysql -uisucon -pisucon isuports < /tmp/tmp.sql
-mysql -u"$ISUCON_DB_USER" \
-		-p"$ISUCON_DB_PASSWORD" \
-		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT" \
-		"$ISUCON_DB_NAME" < /tmp/tmp.sql
+mysql -uisucon -pisucon isuports < /tmp/tmp.sql
