51466796dd872d6da1d236970f71527302973400
ktny <katsunori.t10@gmail.com>
2022-07-23 13:03:02 +0900

    fix

--- sql/init.sh
+++ sql/init.sh
@@ -33,7 +33,7 @@ rm -f /tmp/tmp.sql
 
 for i in {1..234}; do
     echo start $i.db
-    /home/isucon/webapp/sql/sqlite3-to-sql /home/isucon/webapp/tenant_db/$i.db >> /tmp/tmp.sql
+    ./sqlite3-to-sql ../tenant_db/$i.db >> /tmp/tmp.sql
 done
 
 mysql -u"$ISUCON_DB_USER" \
