9a5e2a10dab699a8c5bfafdcf6e627873bab44b2
ktny <katsunori.t10@gmail.com>
2022-07-23 11:24:54 +0900

    fix

--- sql/sqlite3-to-mysql.sh
+++ sql/sqlite3-to-mysql.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -x pipefail
 
+rm -f /tmp/tmp.sql
+
 for i in {1..234}; do
     echo start $i.db
-    # rm -f /tmp/tmp.sql
     ./sqlite3-to-sql ../tenant_db/$i.db >> /tmp/tmp.sql
 done
 
