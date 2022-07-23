d0aa91122fa746ffeba9c2371e642bbc4888d8dd
ktny <katsunori.t10@gmail.com>
2022-07-23 09:19:12 +0900

    add index

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -27,5 +27,6 @@ CREATE TABLE `visit_history` (
   `competition_id` VARCHAR(255) NOT NULL,
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
-  INDEX `tenant_id_idx` (`tenant_id`, `competition_id`)
+  INDEX `tenant_id_idx` (`tenant_id`, `competition_id`),
+  INDEX `created_at_idx` (`created_at`),
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
