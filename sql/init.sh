16159413276633c6f8614d4eb90ba73cd9bdf55f
ktny <katsunori.t10@gmail.com>
2022-07-23 13:01:17 +0900

    fix

--- sql/init.sh
+++ sql/init.sh
@@ -29,3 +29,15 @@ mysql -u"$ISUCON_DB_USER" \
 
 # 一部をSQLiteからMySQLに移行
 # ./sqlite3-to-mysql.sh
+rm -f /tmp/tmp.sql
+
+for i in {1..234}; do
+    echo start $i.db
+    /home/isucon/webapp/sql/sqlite3-to-sql /home/isucon/webapp/tenant_db/$i.db >> /tmp/tmp.sql
+done
+
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < /tmp/tmp.sql
