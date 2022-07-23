fd2a7f7af71b50217e1142888ba09b38a3fa11b9
ktny <katsunori.t10@gmail.com>
2022-07-23 13:10:58 +0900

    add patch

--- sql/init.sh
+++ sql/init.sh
@@ -41,3 +41,9 @@ mysql -u"$ISUCON_DB_USER" \
 		--host "$ISUCON_DB_HOST" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < /tmp/tmp.sql
+
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < patch.sql
