62c616073007b9cae21b93f1795f971b47c229b0
ktny <katsunori.t10@gmail.com>
2022-07-31 19:18:36 +0900

    visit_historyを作り直す

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -29,3 +29,9 @@ CREATE TABLE `visit_history` (
   `updated_at` BIGINT NOT NULL,
   INDEX `tenant_id_idx` (`tenant_id`, `competition_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+
+-- visit_historyはtenant_id, player_id, competition_idごとの最初のレコードしかいらないのでそのようなテーブルに作り直す
+-- create table visit_history_2 as select tenant_id, player_id, competition_id, MIN(created_at) as created_at from visit_history group by tenant_id, player_id, competition_id;
+-- alter table visit_history rename to visit_history_old;
+-- alter table visit_history_2 rename to visit_history;
+-- alter table visit_history add unique index visit_history_idx (tenant_id, competition_id, player_id);
